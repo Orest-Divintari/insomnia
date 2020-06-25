bbd9a3074944d5084ce944c7536f45968207d64b
= <o.divintari@gmail.com>
2020-06-25 09:44:11 +0200

    fix: add documentation of index function

--- app/Http/Controllers/Api/ReplyController.php
+++ app/Http/Controllers/Api/ReplyController.php
@@ -9,14 +9,16 @@ use App\Thread;
 class ReplyController extends Controller
 {
 
+    /**
+     * Display paginated list of replies
+     *
+     * @param Thread $thread
+     * @return \Illuminate\Http\Response
+     */
     public function index(Thread $thread)
     {
         return $thread->replies()->paginate(Reply::PER_PAGE);
 
     }
 
-    public function show()
-    {
-        return response(200);
-    }
 }
