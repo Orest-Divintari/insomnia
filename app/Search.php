c17052ca485dbdd147d9c4a6d9884b2df85613c9
= <o.divintari@gmail.com>
2020-09-17 23:04:19 +0200

    feature: handles the case where the result is empty or an exception is thrown

--- app/Search.php
+++ app/Search.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Exception;
+
 class Search
 {
     /**
@@ -12,12 +14,22 @@ class Search
 
     public function getResults()
     {
-        if (request('type') == 'thread') {
-            $results = app(SearchThreads::class)->query();
-        } elseif (request('type') == 'profile_post') {
-            $results = app(SearchProfilePosts::class)->query();
-        } elseif (request()->missing('type')) {
-            $results = app(SearchAllPosts::class)->query();
+        try
+        {
+            if (request('type') == 'thread') {
+                $results = app(SearchThreads::class)->query();
+            } elseif (request('type') == 'profile_post') {
+                $results = app(SearchProfilePosts::class)->query();
+            } elseif (request()->missing('type')) {
+                $results = app(SearchAllPosts::class)->query();
+            }
+        } catch (Exception $e) {
+
+            return 'No results';
+        }
+
+        if (is_null($results)) {
+            return 'No results';
         }
 
         return $results->paginate(static::RESULTS_PER_PAGE);
