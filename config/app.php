3294e37b294c9dc680cffcdebcacf98252a81f05
= <o.divintari@gmail.com>
2020-10-18 09:55:09 +0200

    Fix regiter the search and filter services

--- config/app.php
+++ config/app.php
@@ -174,6 +174,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /**
+         * Other Service Providers
+         */
+        App\Providers\SearchServiceProvider::class,
+        App\Providers\FilterServiceProvider::class,
 
     ],
 
