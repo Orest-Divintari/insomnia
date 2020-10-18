74aa346de03bd95c6aaae5a2f3f37981e7e2caa1
= <o.divintari@gmail.com>
2020-10-18 09:20:54 +0200

    Refactor store the request type in a variable and use that in the if else statements

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -46,11 +46,12 @@ class SearchController extends Controller
      */
     public function getSearchType()
     {
-        if (request('type') == 'thread') {
+        $type = $this->request->input('type');
+        if ($type == 'thread') {
             return view('search.advanced.threads', ['type' => 'thread']);
-        } elseif (request('type') == 'profile_post') {
+        } elseif ($type == 'profile_post') {
             return view('search.advanced.profile_posts', ['type' => 'profile_post']);
-        } elseif (request('type') == 'tags') {
+        } elseif ($type == 'tags') {
             return view('search.advanced.tags', ['type' => 'tags']);
         }
         return view('search.advanced.all_posts', ['type' => '']);
