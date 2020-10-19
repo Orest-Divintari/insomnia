4117c8258a9d94a0b17a54d06ff8192eac6ec55c
= <o.divintari@gmail.com>
2020-10-19 11:24:52 +0200

    Fix rename file

--- tests/Unit/PostFiltersTest.php
+++ tests/Unit/PostFiltersTest.php
@@ -2,21 +2,21 @@
 
 namespace Tests\Unit;
 
-use App\Filters\Filters;
+use App\Filters\PostFilters;
 use App\Thread;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class FiltersTest extends TestCase
+class PostFiltersTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
     public function when_a_user_searches_for_records_given_a_username_that_does_not_exist_then_throw_error()
     {
-        $filter = app(Filters::class, ['builder' => Thread::query()]);
-
+        $filter = app(PostFilters::class);
+        $filter->setBuilder(Thread::query());
         $this->expectException(ModelNotFoundException::class);
         $filter->postedBy('random name');
     }
