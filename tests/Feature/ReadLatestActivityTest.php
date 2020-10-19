242396926a54050cf9da395c37573783a56086c0
= <o.divintari@gmail.com>
2020-10-19 09:30:13 +0200

    Fix make code more compact to improve readability

--- tests/Feature/ReadLatestActivityTest.php
+++ tests/Feature/ReadLatestActivityTest.php
@@ -19,36 +19,57 @@ class ReadLatestActivityTest extends TestCase
 
         $numberOfActivities = Activity::NUMBER_OF_ACTIVITIES;
 
-        createMany(Thread::class, $numberOfActivities, ['user_id' => $profileOwner->id]);
+        createMany(
+            Thread::class,
+            $numberOfActivities,
+            ['user_id' => $profileOwner->id]
+        );
 
-        $this->assertCount($numberOfActivities, $profileOwner->activities);
+        $this->assertCount(
+            $numberOfActivities,
+            $profileOwner->activities
+        );
 
         $user = $this->signIn();
 
-        $response = $this->get(route('api.latest-activity.index', $profileOwner))->json();
+        $response = $this->get(
+            route('api.latest-activity.index', $profileOwner)
+        )->json();
 
-        $this->assertCount($numberOfActivities, $response['data']);
+        $this->assertCount(
+            $numberOfActivities,
+            $response['data']
+        );
     }
 
     /** @test */
     public function a_user_can_read_only_the_posting_activities()
     {
         $profileOwner = $this->signIn();
-
         $numberOfPostingActivities = 5;
-
-        createMany(Thread::class, $numberOfPostingActivities, ['user_id' => $profileOwner->id]);
+        createMany(
+            Thread::class,
+            $numberOfPostingActivities,
+            ['user_id' => $profileOwner->id]
+        );
 
         $thread = Thread::first();
-
-        $reply = $thread->addReply(raw(Reply::class, ['user_id' => $profileOwner->id]));
-
+        $reply = $thread->addReply(
+            raw(Reply::class,
+                ['user_id' => $profileOwner->id])
+        );
         $numberOfPostingActivities++;
-
         $reply->likedBy($profileOwner);
 
-        $response = $this->get(route('api.latest-activity.index', [$profileOwner, 'postings' => true]))->json();
+        $response = $this->get(
+            route(
+                'api.latest-activity.index', [
+                    $profileOwner, 'postings' => true]
+            ))->json();
 
-        $this->assertCount($numberOfPostingActivities, $response['data']);
+        $this->assertCount(
+            $numberOfPostingActivities,
+            $response['data']
+        );
     }
 }
