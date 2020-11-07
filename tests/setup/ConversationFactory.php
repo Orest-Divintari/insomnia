a01987153b685ecd52ca363c63dc1cfb36142b07
= <o.divintari@gmail.com>
2020-11-07 15:02:41 +0100

    Test create a conversation factory
    this conversation factory class creates a conversatio, adds a participant and a message in order to have a full conversation between 2 users for testing purposes

---
+++ tests/setup/ConversationFactory.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Setup;
+
+use App\Conversation;
+use App\Message;
+use App\User;
+
+class ConversationFactory
+{
+
+    protected $participants = [];
+    protected $message;
+    protected $slug;
+
+    protected $user = null;
+
+    public function create($title = null)
+    {
+        $conversation = create(
+            Conversation::class,
+            ['title' => $title ?: 'some title']
+        );
+
+        if (empty($this->participants)) {
+            $this->withParticipants();
+        }
+        if (is_null($this->message)) {
+            $this->withMessage();
+        }
+
+        $conversation->addParticipants($this->participants);
+        $conversation->addMessage($this->message);
+
+        return $conversation;
+    }
+
+    /**
+     * Set the message for the conversation
+     *
+     * @param string $message
+     * @return ConversationFactory
+     */
+    public function withMessage($message = null)
+    {
+        $this->message = $message ?: 'some message';
+        return $this;
+    }
+
+    /**
+     * Set the username of an existing user
+     *
+     * @param User $user
+     * @return ConversationFactory
+     */
+    public function by($user)
+    {
+        $this->user = $user;
+        // $username = $username ?: 'George';
+        // $user = create(User::class, ['name' => $username]);
+        // $this->user = $this->signIn($user);
+        return $this;
+    }
+
+    /**
+     * Set the name of existing participants
+     *
+     * @param string[] $participants
+     * @return ConversationFactory
+     */
+    public function withParticipants($participants = [])
+    {
+        if (empty($participants)) {
+            $this->participants = [create(User::class)->name];
+        } else {
+            $this->participants = $participants;
+        }
+
+        return $this;
+    }
+
+}
