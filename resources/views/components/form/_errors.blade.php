10a4393d5b058a64d0d55dddd69d3a1bcefc7a40
= <o.divintari@gmail.com>
2020-11-16 10:37:18 +0100

    Fix remove empty line

--- resources/views/components/form/_errors.blade.php
+++ resources/views/components/form/_errors.blade.php
@@ -3,7 +3,6 @@
     @foreach($errors->all() as $error)
     <div
         class="w-full py-4 bg-red-alert rounded mb-3 shadow-md border-l-2 border-red-alert-text font-light text-red-alert-text pl-5 flex items-center">
-
         <span class="fas fa-ban mr-3"></span>
         <p>{{ $error }}</p>
     </div>
