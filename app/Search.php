ef3427368eeca86d9bda0bc38edc2bf09b6e7d96
= <o.divintari@gmail.com>
2020-11-16 10:41:00 +0100

    Fix rename the query parameter

--- app/Search.php
+++ app/Search.php
@@ -55,7 +55,7 @@ class Search
     public function handle(Request $request)
     {
         $type = $request->input('type') ?: '';
-        $onlyTitle = $request->boolean('only_title') ?: false;
+        $onlyTitle = $request->boolean('onlyTitle') ?: false;
         $searchQuery = $request->input('q') ?: '';
 
         $index = $this->indexFor(
