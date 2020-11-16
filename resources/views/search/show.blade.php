401daf943f78f6ac2caf0ebdfe6ca5f64e30b5e0
= <o.divintari@gmail.com>
2020-11-16 10:43:31 +0100

    Refactor display the errors if any or the results if any

--- resources/views/search/show.blade.php
+++ resources/views/search/show.blade.php
@@ -1,8 +1,20 @@
 <x-layouts._forum>
+    @if($errors->any())
+    <h1 class="text-3xl font-bold">Oops! We ran into some problems.<span class="text-blue-mid italic"> {{ $query }}
+        </span></h1>
+    <p class="mt-5 bg-white text-sm text-black-semi p-4 shadow-lg rounded"> {{ $errors->first() }} </p>
+    @elseif(is_string($results))
+    <h1 class="text-3xl font-bold">Insomnia Forum</h1>
+    <p class="mt-5 bg-white text-sm text-black-semi p-4 shadow-lg rounded">{{ $results }}</p>
+    @else
     <div class="py-12 -mx-5 -mt-5 bg-gallery"></div>
-    <div class="section">
+    <div class="section mb-1">
+        @if(isset($query))
         <h1 class="text-3xl font-bold">Search results for query: <span class="text-blue-mid italic"> {{ $query }}
-            </span></h1>
+            </span> </h1>
+        @else
+        <h1 class="text-3xl font-bold">Search results</h1>
+        @endif
     </div>
     <div class="section">
         <x-breadcrumb.container>
@@ -11,10 +23,8 @@
             </x-breadcrumb.leaf>
         </x-breadcrumb.container>
     </div>
-    @if(is_string($results))
-    <h1 class="section-title">{{ $results }}</h1>
-    @else
     <search-results :dataset="{{ $results->toJson() }}" query="{{ $query }}"></search-results>
     @endif
+
 
 </x-layouts._forum>
