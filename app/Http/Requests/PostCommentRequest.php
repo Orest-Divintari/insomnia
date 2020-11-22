29688fb79dc344b19d6933b1b19a9667f96956ba
= <o.divintari@gmail.com>
2020-11-22 10:24:53 +0100

    refactor: add costum error messages

--- app/Http/Requests/PostCommentRequest.php
+++ app/Http/Requests/PostCommentRequest.php
@@ -24,7 +24,20 @@ class PostCommentRequest extends FormRequest
     public function rules()
     {
         return [
-            'body' => 'required',
+            'body' => ['string', 'required'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'body.required' => 'Please enter a valid message.',
+            'body.string' => 'Please enter a valid message.',
         ];
     }
 }
