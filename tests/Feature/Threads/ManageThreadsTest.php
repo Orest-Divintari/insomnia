2de72e300e4086b7f2ddd39b592c3c8f05c3f525
= <o.divintari@gmail.com>
2020-11-22 13:02:51 +0100

    Test: ensure that custom message is returned when validation fails

--- tests/Feature/Threads/ManageThreadsTest.php
+++ tests/Feature/Threads/ManageThreadsTest.php
@@ -11,6 +11,7 @@ class ManageThreadsTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $errorMessage = 'Please enter a valid title.';
     /** @test */
     public function non_authorized_users_may_not_update_the_title_of_a_thread()
     {
@@ -47,6 +48,55 @@ class ManageThreadsTest extends TestCase
 
         $this->assertDatabaseHas('threads', $newTitle);
 
+    }
+
+    /** @test */
+    public function when_updating_a_thread_a_title_is_required()
+    {
+        $user = $this->signIn();
+
+        $thread = create(Thread::class, [
+            'title' => 'old title',
+            'user_id' => $user->id,
+        ]);
+
+        $emptyTitle = [
+            'title' => '',
+        ];
+
+        $this->assertDatabaseHas('threads', [
+            'id' => $thread->id,
+            'title' => 'old title',
+        ]);
+
+        $this->patchJson(route('api.threads.update', $thread), $emptyTitle)
+            ->assertStatus(422)
+            ->assertJson(['title' => [$this->errorMessage]]);
+    }
+
+    /** @test */
+    public function when_updating_a_thread_the_title_must_be_of_type_string()
+    {
+
+        $user = $this->signIn();
+
+        $thread = create(Thread::class, [
+            'title' => 'old title',
+            'user_id' => $user->id,
+        ]);
+
+        $nonStringTitle = [
+            'title' => array(15),
+        ];
+
+        $this->assertDatabaseHas('threads', [
+            'id' => $thread->id,
+            'title' => 'old title',
+        ]);
+
+        $this->patchJson(route('api.threads.update', $thread), $nonStringTitle)
+            ->assertStatus(422)
+            ->assertJson(['title' => [$this->errorMessage]]);
     }
 
 }
