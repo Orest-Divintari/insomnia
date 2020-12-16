8f2271b6e99f0e9506429b34aa127b8f9ef38651
= <o.divintari@gmail.com>
2020-12-16 13:22:11 +0100

    Test: ensure that threads related to a tag are displayed

---
+++ tests/Feature/ViewTagTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Tag;
+use App\Thread;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewTagTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function display_the_threads_that_are_correlated_with_a_tag()
+    {
+        $this->withoutExceptionHandling();
+        $threads = createMany(Thread::class, 2);
+        $tag = create(Tag::class);
+
+        foreach ($threads as $thread) {
+            $thread->addTags([$tag->name]);
+        }
+
+        $this->assertCount(2, $tag->fresh()->threads);
+        $threads = $threads->toArray();
+
+        $this->get(route('tags.show', $tag))
+            ->assertSee($threads[0]['title'])
+            ->assertSee($threads[1]['title']);
+
+    }
+}
