5533c39f5bfe73ff7b1655fb7f349f0b82ac0d55
= <o.divintari@gmail.com>
2020-12-19 13:17:24 +0100

    Test: ensure that event is fired when a user unfollows another user

---
+++ tests/Feature/Follows/AUserUnfollowedYouTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Follows;
+
+use App\Listeners\Follow\DeleteFollowNotification;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery;
+use Tests\TestCase;
+
+class AUserUnfollowedYouTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function when_a_user_unfollows_another_user_then_an_event_is_fired()
+    {
+        $followerUser = $this->signIn();
+        $listener = Mockery::spy(DeleteFollowNotification::class);
+        app()->instance(DeleteFollowNotification::class, $listener);
+        $followingUser = create(User::class);
+        $followerUser->follow($followingUser);
+        $this->assertCount(1, $followingUser->notifications);
+
+        $this->delete(route('api.follow.destroy', $followingUser));
+
+        $listener->shouldHaveReceived('handle')
+            ->with(Mockery::on(function ($event) use ($followingUser, $followerUser) {
+                return $event->follower->id == $followerUser->id
+                && $event->following->id == $followingUser->id;
+            }));
+    }
+}
