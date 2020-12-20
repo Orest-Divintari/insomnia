a31f08cae1bf91510a92cac295af803df1b0bc10
= <o.divintari@gmail.com>
2020-12-20 15:22:01 +0100

    Refactor: change the elements structurre to fit the notification message

--- resources/js/components/notifications/MessageLikeNotification.vue
+++ resources/js/components/notifications/MessageLikeNotification.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <a
-      @click="showProfile(notificationData.liker)"
-      class="blue-link notification-profile"
-      >{{ notificationData.liker.name }}</a
-    >
-    <div
-      @click="showMessage(notificationData.message)"
-      class="inline notification-content w-full"
-    >
-      liked your message
-      <a class="blue-link">{{ notificationData.conversation.title }}</a>
-      <p class="text-xs text-gray-lightest">
-        {{ notificationData.like.date_created }}
-      </p>
+    <div class="flex items-start">
+      <img
+        @click="showProfile(notificationData.liker)"
+        :src="notificationData.liker.avatar_path"
+        class="avatar-sm mr-2"
+      />
+      <div>
+        <a
+          @click="showProfile(notificationData.liker)"
+          class="blue-link notification-profile mr-1/2"
+          >{{ notificationData.liker.name }}</a
+        >
+        <span class="group" @click="showMessage(notificationData.message)">
+          liked your message in the conversation
+          <a class="group-hover:underline blue-link">
+            {{ notificationData.conversation.title }}
+          </a>
+          <p class="block text-xs text-gray-lightest">
+            {{ notificationData.like.date_created }}
+          </p>
+        </span>
+      </div>
     </div>
   </div>
 </template>
