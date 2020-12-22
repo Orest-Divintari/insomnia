51ae3e4324c6c03e6df06ba29d3f9db725c9219b
= <o.divintari@gmail.com>
2020-12-22 12:42:27 +0100

    Test: rename the test file

--- tests/Feature/Threads/ViewThreadsTest.php
+++ tests/Feature/Threads/ViewThreadsTest.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ReadThreadsTest extends TestCase
+class ViewThreadsTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -21,18 +21,16 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_read_a_single_thread()
+    public function a_user_can_view_a_single_thread()
     {
         $this->get(route('threads.show', $this->thread))
             ->assertSee($this->thread->title);
-
     }
 
     /** @test */
-    public function a_user_can_read_the_threads_associated_with_a_category()
+    public function a_user_can_view_the_threads_associated_with_a_category()
     {
         $category = create(Category::class);
-
         $thread = create(Thread::class, ['category_id' => $category->id]);
 
         $this->get(route('threads.index', $category))
@@ -40,18 +38,19 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_read_the_paginated_threads_associated_with_a_category()
+    public function a_user_can_view_the_paginated_threads_associated_with_a_category()
     {
         $category = create(Category::class);
-
         createMany(Thread::class, 100, [
             'category_id' => $category->id,
         ]);
 
         $response = $this->getJson(route('api.threads.index', $category))->json();
+
         $this->assertCount(Thread::PER_PAGE, $response['data']);
 
         $response = $this->getJson(route('api.threads.index', $category) . '?page=2')->json();
+
         $this->assertCount(Thread::PER_PAGE, $response['data']);
     }
 
@@ -59,20 +58,16 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_go_to_a_specific_reply_on_a_paginated_thread()
     {
         $thread = create(Thread::class, ['replies_count' => 30]);
-
         createMany(Reply::class, 30, [
             'repliable_id' => $thread->id,
             'repliable_type' => Thread::class,
         ]);
         $reply = Reply::find(15);
-
         $numberOfRepliesBefore = $thread->replies()->where('id', '<=', $reply->id)->count();
-
         $pageNumber = (int) ceil($numberOfRepliesBefore / Thread::REPLIES_PER_PAGE);
 
         $this->get(route('api.replies.show', $reply))
             ->assertSee($reply->title);
-
     }
 
     /** @test */
@@ -88,7 +83,6 @@ class ReadThreadsTest extends TestCase
         $threadByUric = create(Thread::class, [
             'user_id' => $uric->id,
         ]);
-
         $anotherUser = create(User::class);
         $threadByAnotherUser = create(
             Thread::class,
@@ -98,11 +92,10 @@ class ReadThreadsTest extends TestCase
         $this->get(route('filtered-threads.index') . "?postedBy={$uric->name}")
             ->assertSee($threadByUric->title)
             ->assertDontSee($threadByAnotherUser->title);
-
     }
 
     /** @test */
-    public function user_can_read_the_threads_ordered_by_date_of_creation_in_descending_order()
+    public function user_can_view_the_threads_ordered_by_date_of_creation_in_descending_order()
     {
         $oldThread = create(Thread::class, [
             'created_at' => Carbon::now()->subDay(),
@@ -110,6 +103,7 @@ class ReadThreadsTest extends TestCase
         ]);
 
         $response = $this->getJson(route('filtered-threads.index') . '?newThreads=1');
+
         $this->assertEquals($this->thread->id, $response['data'][0]['id']);
         $this->assertEquals($oldThread->id, $response['data'][1]['id']);
     }
@@ -119,13 +113,14 @@ class ReadThreadsTest extends TestCase
     {
         $recentlyActiveThread = $this->thread;
         $recentlyActiveThread->addReply(raw(Reply::class));
-
         $inactiveThread = create(Thread::class, [
             'updated_at' => Carbon::now()->subDay(),
         ]);
         $inactiveThread->addReply(raw(Reply::class));
 
-        $threads = $this->getJson(route('filtered-threads.index') . "?newPosts=1")->json()['data'];
+        $threads = $this->getJson(
+            route('filtered-threads.index') . "?newPosts=1"
+        )->json()['data'];
 
         $this->assertCount(2, $threads);
         $this->assertEquals($recentlyActiveThread->id, $threads[0]['id']);
@@ -133,22 +128,18 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_read_the_threads_that_has_replied_to()
+    public function a_user_can_view_the_threads_that_has_replied_to()
     {
         $anotherUser = $this->signIn();
-
         $threadWithoutParticipation = create(Thread::class);
         $threadWithoutParticipation->addReply(
             raw(Reply::class, [
                 'user_id' => $anotherUser,
             ])
         );
-
         $threadWithoutReplies = create(Thread::class);
-
         $user = create(User::class, ['name' => 'orestis']);
         $this->signIn($user);
-
         $thread = create(Thread::class);
         $thread->addReply(
             raw(Reply::class, [
@@ -167,24 +158,32 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function user_can_read_trending_threads()
+    public function user_can_view_trending_threads()
     {
         $this->thread->addReply(raw(Reply::class));
         $this->thread->addReply(raw(Reply::class));
         $this->thread->addReply(raw(Reply::class));
         $this->thread->update(['views' => 50]);
-
         $lessTrendingThread = create(Thread::class);
         $lessTrendingThread->addReply(raw(Reply::class));
         $lessTrendingThread->update(['views' => 100]);
 
-        $response = $this->getJson(route('filtered-threads.index') . "?trending=1");
-        $this->assertEquals($this->thread->id, $response['data'][0]['id']);
-        $this->assertEquals($lessTrendingThread->id, $response['data'][1]['id']);
+        $response = $this->getJson(
+            route('filtered-threads.index') . "?trending=1"
+        );
+
+        $this->assertEquals(
+            $this->thread->id,
+            $response['data'][0]['id']
+        );
+        $this->assertEquals(
+            $lessTrendingThread->id,
+            $response['data'][1]['id']
+        );
     }
 
     /** @test */
-    public function user_can_read_the_unanswered_threads()
+    public function user_can_view_the_unanswered_threads()
     {
         $threadWithReplies = create(Thread::class);
         $threadWithReplies->addReply(raw(Reply::class));
@@ -195,10 +194,9 @@ class ReadThreadsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_read_the_threads_tha_has_subscribred_to()
+    public function a_user_can_view_the_threads_tha_has_subscribred_to()
     {
         $threadThatHasntSubscribedTo = create(Thread::class);
-
         $user = create(User::class, [
             'name' => 'jorgo',
         ]);
@@ -208,40 +206,38 @@ class ReadThreadsTest extends TestCase
         $this->get(route('filtered-threads.index') . "?watched=1")
             ->assertSee($this->thread->title)
             ->assertDontSee($threadThatHasntSubscribedTo->title);
-
     }
 
     /** @test */
-    public function user_can_read_the_threads_that_have_been_last_updated_X_days_ago()
+    public function user_can_view_the_threads_that_have_been_last_updated_X_days_ago()
     {
         $todaysThread = create(Thread::class);
-
         $oldThread = create(Thread::class, [
             'updated_at' => Carbon::now()->subMonth(),
         ]);
-
         $daysAgo = 3;
         $lastUpdated = Carbon::now()->subDays($daysAgo);
 
-        $this->get(route('filtered-threads.index') . "?lastUpdated=" . $daysAgo)
-            ->assertSee($todaysThread->title)
+        $this->get(
+            route('filtered-threads.index') . "?lastUpdated=" . $daysAgo
+        )->assertSee($todaysThread->title)
             ->assertDontSee($oldThread->title);
 
     }
 
     /** @test */
-    public function user_can_read_the_threads_that_have_been_last_created_X_days_ago()
+    public function user_can_view_the_threads_that_have_been_last_created_X_days_ago()
     {
         $todaysThread = create(Thread::class);
         $oldThread = create(Thread::class, [
             'created_at' => Carbon::now()->subMonth(),
         ]);
-
         $daysAgo = 3;
         $lastUpdated = Carbon::now()->subDays($daysAgo);
 
-        $this->get(route('filtered-threads.index') . "?lastCreated=" . $daysAgo)
-            ->assertSee($todaysThread->title)
+        $this->get(
+            route('filtered-threads.index') . "?lastCreated=" . $daysAgo
+        )->assertSee($todaysThread->title)
             ->assertDontSee($oldThread->title);
 
     }
@@ -262,7 +258,6 @@ class ReadThreadsTest extends TestCase
     public function guests_cannot_mark_a_thread_as_read()
     {
         $user = create(User::class);
-
         $thread = create(Thread::class);
         $this->assertTrue($thread->hasBeenUpdated);
 
@@ -289,6 +284,5 @@ class ReadThreadsTest extends TestCase
 
         $this->get(route('filtered-threads.index'))
             ->assertSee($pinnedThread->title);
-
     }
 }
