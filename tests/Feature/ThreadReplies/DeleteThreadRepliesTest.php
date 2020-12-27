c5b30a26730ef29fb3f89f88877c05dfca45d4eb
= <o.divintari@gmail.com>
2020-12-27 10:17:24 +0100

    Refactor: split test files from manage to delete and update

---
+++ tests/Feature/ThreadReplies/DeleteThreadRepliesTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\ThreadReplies;
+
+use App\Thread;
+use Facades\Tests\Setup\ReplyFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class DeleteThreadRepliesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function authorized_users_may_delete_a_reply()
+    {
+        $replyPoster = $this->signIn();
+        $reply = ReplyFactory::by($replyPoster)->create();
+
+        $this->delete(route('api.replies.destroy', $reply));
+
+        $this->assertDatabaseMissing('replies', [
+            'id' => $reply->id,
+            'repliable_type' => Thread::class,
+        ]);
+    }
+
+    /** @test */
+    public function unauthorized_users_cannot_delete_a_reply()
+    {
+        $reply = ReplyFactory::create();
+        $unauthorizedUser = $this->signIn();
+
+        $response = $this->delete(route('api.replies.destroy', $reply));
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+        $this->assertDatabaseHas('replies', [
+            'id' => $reply->id,
+        ]);
+    }
+}
--- tests/Feature/ThreadReplies/UpdateThreadRepliesTest.php
+++ tests/Feature/ThreadReplies/UpdateThreadRepliesTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
-class UpdateReplyTest extends TestCase
+class UpdateThreadRepliesTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -80,34 +80,6 @@ class UpdateReplyTest extends TestCase
 
         $response->assertStatus(422)
             ->assertJson(['body' => [$this->errorMessage]]);
-    }
-
-    /** @test */
-    public function authorized_users_may_delete_a_reply()
-    {
-        $replyPoster = $this->signIn();
-        $reply = ReplyFactory::by($replyPoster)->create();
-
-        $this->delete(route('api.replies.destroy', $reply));
-
-        $this->assertDatabaseMissing('replies', [
-            'id' => $reply->id,
-            'repliable_type' => Thread::class,
-        ]);
-    }
-
-    /** @test */
-    public function unauthorized_users_cannot_delete_a_reply()
-    {
-        $reply = ReplyFactory::create();
-        $unauthorizedUser = $this->signIn();
-
-        $response = $this->delete(route('api.replies.destroy', $reply));
-
-        $response->assertStatus(Response::HTTP_FORBIDDEN);
-        $this->assertDatabaseHas('replies', [
-            'id' => $reply->id,
-        ]);
     }
 
 }
