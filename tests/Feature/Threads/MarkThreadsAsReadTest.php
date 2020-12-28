2820a011f95d21adab74e6dac8292d00d9fe4be1
= <o.divintari@gmail.com>
2020-12-28 15:45:08 +0100

    Test: ensure that a thread is marked as read

---
+++ tests/Feature/Threads/MarkThreadsAsReadTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature\Threads;
+
+use App\Category;
+use App\Thread;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class MarkThreadsAsReadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function when_an_authenticated_user_visits_a_thread_then_the_thread_is_marked_as_read()
+    {
+        $category = create(Category::class);
+        $threads = createMany(Thread::class, 2, ['category_id' => $category->id]);
+        $readThread = $threads->first();
+        $user = $this->signIn();
+
+        $this->get(route('threads.show', $readThread));
+
+        $threads = $this->getJson(route('threads.index', $category))->json()['data'];
+        $threads = collect($threads);
+        $this->assertTrue(
+            $threads->every(function ($thread, $key) use ($readThread) {
+                if ($thread['id'] == $readThread->id) {
+                    return $thread['has_been_updated'] == false;
+                }
+                return $thread['has_been_updated'] == true;
+            })
+        );
+    }
+
+    /** @test */
+    public function when_a_guest_visits_a_thread_it_is_not_marked_as_read()
+    {
+        $category = create(Category::class);
+        $threads = createMany(Thread::class, 2, ['category_id' => $category->id]);
+        $visitedThread = $threads->first();
+
+        $this->get(route('threads.show', $visitedThread));
+        $threads = $this->getJson(route('threads.index', $category))->json()['data'];
+
+        $threads = collect($threads);
+        $this->assertTrue(
+            $threads->every(function ($thread, $key) use ($visitedThread) {
+                return $thread['has_been_updated'] == true;
+            })
+        );
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_mark_a_thread_as_read()
+    {
+        $user = $this->signIn();
+        $thread = create(Thread::class);
+        $this->assertTrue($thread->hasBeenUpdated());
+
+        $this->patch(route('api.read-threads.update', $thread));
+
+        $this->assertFalse($thread->hasBeenUpdated());
+    }
+
+    /** @test */
+    public function guests_cannot_mark_a_thread_as_read()
+    {
+        $user = create(User::class);
+        $thread = create(Thread::class);
+        $this->assertTrue($thread->hasBeenUpdated());
+
+        $this->patch(route('api.read-threads.update', $thread))
+            ->assertRedirect('login');
+
+        $this->assertTrue($thread->hasBeenUpdated());
+    }
+}
