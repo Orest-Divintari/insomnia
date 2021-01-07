5e28b2d0743e25ce42628c99f0cdd0d1c7e7ce2c
= <o.divintari@gmail.com>
2021-01-07 17:11:09 +0100

    Fix: add parenthesis to call the method any

--- resources/views/components/form/_errors.blade.php
+++ resources/views/components/form/_errors.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-2 mb-6">
-    @if($errors->any)
+    @if($errors->any())
     @foreach($errors->all() as $error)
     <div
         class="w-full py-4 bg-red-alert rounded mb-3 shadow-md border-l-2 border-red-alert-text font-light text-red-alert-text pl-5 flex items-center">
