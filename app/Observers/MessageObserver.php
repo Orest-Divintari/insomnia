0b410f609b547ba3c527f3717ef4fd31e88f6e92
= <o.divintari@gmail.com>
2021-01-12 09:50:23 +0100

    Refactor: create a dedicated observer for conversation messages

---
+++ app/Observers/MessageObserver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Observers;
+
+use App\Reply;
+
+class MessageObserver
+{
+
+    /**
+     * Handle the reply "created" event.
+     *
+     * @param  \App\Reply  $reply
+     * @return void
+     */
+    public function created(Reply $reply)
+    {
+        $conversation = $reply->repliable;
+        if ($reply->isMessage()) {
+            $conversation->unhide();
+            $reply->poster->read($conversation);
+        }
+    }
+}
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -17,19 +17,4 @@ class ReplyObserver
         $reply->likes->each->delete();
         $reply->activities->each->delete();
     }
-
-    /**
-     * Handle the reply "created" event.
-     *
-     * @param  \App\Reply  $reply
-     * @return void
-     */
-    public function created(Reply $reply)
-    {
-        if ($reply->isMessage()) {
-            $reply->repliable->unhide();
-            $reply->poster->read($reply->repliable);
-        }
-    }
-
 }
