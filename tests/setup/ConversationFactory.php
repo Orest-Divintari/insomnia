721afe7bebdc35cf1b9a2a784c549978f1ecda48
Orestis Divintari <o.divintari@gmail.com>
2021-04-02 19:03:20 +0200

    Test: add functionality to create one or more conversations

--- tests/setup/ConversationFactory.php
+++ tests/setup/ConversationFactory.php
@@ -5,38 +5,168 @@ namespace Tests\Setup;
 use App\Conversation;
 use App\Message;
 use App\User;
+use Faker\Generator as Faker;
+use Tests\Setup\FactoryInterface;
 
-class ConversationFactory
+class ConversationFactory extends Factory implements FactoryInterface
 {
 
+    /**
+     * Faker generator
+     *
+     * @var Faker
+     */
+    protected $faker;
+
+    /**
+     * Participant names
+     *
+     * @var string[]
+     */
     protected $participants = [];
+
+    /**
+     * Conversation message
+     *
+     * @var string
+     */
     protected $message;
+
+    /**
+     * Converastion slug
+     *
+     * @var string
+     */
     protected $slug;
 
+    /**
+     * Conversation starter
+     *
+     * @var User
+     */
     protected $user = null;
 
-    public function create($title = null)
+    /**
+     * Create a new instance
+     *
+     * @param Faker $faker
+     */
+    public function __construct(Faker $faker)
     {
+        $this->faker = $faker;
+    }
 
-        $conversation = create(
-            Conversation::class,
-            [
-                'title' => $title ?: 'some title',
-                'user_id' => $this->user ? $this->user->id : auth()->id(),
-            ]
-        );
+    /**
+     * Create a new conversation
+     *
+     * @param string $title
+     * @return Conversation
+     */
+    public function create($attributes = [])
+    {
+        $this->attributes = $attributes;
 
+        $conversation = $this->createConversation($attributes);
+
+        $this->addParticipants($conversation);
+
+        $this->addMessage($conversation);
+
+        $this->resetAttributes();
+
+        return $conversation;
+    }
+
+    /**
+     * Create many conversations
+     *
+     * @param integer $count
+     * @param array $attributes
+     * @return Collection
+     */
+    public function createMany($count = 1, $attributes = [])
+    {
+        $this->attributes = $attributes;
+
+        $conversations = $this->createConversation($attributes, $count);
+
+        $conversations->each(function ($conversation) {
+            $this->addParticipants($conversation);
+            $this->addMessage($conversation);
+        });
+
+        $this->resetAttributes();
+
+        return $conversations;
+    }
+
+    /**
+     * Create one or many conversation instances
+     *
+     * @param $attributes[]
+     * @param integer $count
+     * @return Conversation||Collection
+     */
+    protected function createConversation($attributes, $count = 1)
+    {
+        $conversations = factory(Conversation::class, $count)->create(
+            array_merge(
+                [
+                    'user_id' => $this->userId(),
+                    'title' => $this->faker->sentence(),
+                ],
+                $attributes
+            ));
+
+        if ($count == 1) {
+            return $conversations->first();
+        }
+
+        return $conversations;
+    }
+
+    /**
+     * Add participants to the given conversation
+     *
+     * @param Conversation $conversation
+     * @return void
+     */
+    protected function addParticipants($conversation)
+    {
         if (empty($this->participants)) {
             $this->withParticipants();
         }
+
+        if (!auth()->check()) {
+            $this->participants[] = $this->user->name;
+        }
+
+        $conversation->addParticipants($this->participants);
+    }
+
+    /**
+     * Add a message to the given conversation
+     *
+     * @param Conversation $conversation
+     * @return void
+     */
+    protected function addMessage($conversation)
+    {
         if (is_null($this->message)) {
             $this->withMessage();
         }
 
-        $conversation->addParticipants($this->participants);
         $conversation->addMessage($this->message, $this->user);
+    }
 
-        return $conversation;
+    /**
+     * Get the user id
+     *
+     * @return int
+     */
+    protected function userId()
+    {
+        return $this->user ? $this->user->id : auth()->id();
     }
 
     /**
@@ -47,7 +177,7 @@ class ConversationFactory
      */
     public function withMessage($message = null)
     {
-        $this->message = $message ?: 'some message';
+        $this->message = $message ?: app(Faker::class)->sentence();
         return $this;
     }
 
@@ -76,7 +206,6 @@ class ConversationFactory
         } else {
             $this->participants = $participants;
         }
-
         return $this;
     }
 
