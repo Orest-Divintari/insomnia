c385a4a0b28af3da0de95d40e23ac03a53a962ff
Orestis Divintari <o.divintari@gmail.com>
2021-04-04 12:22:33 +0200

    Fix: truncate thread title

--- resources/views/components/categories/recently-active-thread.blade.php
+++ resources/views/components/categories/recently-active-thread.blade.php
@@ -1,15 +1,16 @@
 <profile-popover :user="{{ $thread->recentReply->poster }}" trigger="avatar" trigger-classes="avatar-md">
 </profile-popover>
-<div class="ml-2 leading-snug flex-1">
-    <p href="{{route('threads.show', $thread->slug) }}" class=" text-smaller font-extrabold tracking-wide">
-        {{ $thread->shortTitle ?: '' }}
-        <span class="text-xs text-gray-lightest font-hairline">...</span>
-    </p>
-    <div class="flex items-center justify-around text-smaller text-gray-lightest font-hairline">
+<div class="ml-2 leading-snug flex-1 truncate mr-2">
+    <a href="{{ route('threads.show', $thread->slug) }}"
+        class="block text-smaller font-extrabold tracking-wide hover:text-blue-mid-dark hover:underline truncate">
+        {{ $thread->title ?: '' }}
+    </a>
+    <div class="flex items-center justify-start text-smaller text-gray-lightest font-hairline">
         <p>{{ $thread->date_updated ?: '' }}
         </p>
         <p class="dot"></p>
-        <profile-popover :user="{{ $thread->recentReply->poster }}" trigger-classes="text-smaller text-gray-lightest">
+        <profile-popover class="min-w-0" :user="{{ $thread->recentReply->poster }}"
+            trigger-classes="text-smaller text-gray-lightest">
         </profile-popover>
 
     </div>
