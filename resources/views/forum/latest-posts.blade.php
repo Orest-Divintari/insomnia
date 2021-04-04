d1f6617638022e428f6e3f51e6951ced6273ffa5
Orestis Divintari <o.divintari@gmail.com>
2021-04-04 10:40:35 +0200

    Fix: break words to next line when needed

--- resources/views/forum/latest-posts.blade.php
+++ resources/views/forum/latest-posts.blade.php
@@ -13,12 +13,13 @@
             <div class="pl-5/2 flex-1">
                 <a class="text-sm tacking-wide leading-normal font-bold text-blue-mid-dark hover:underline"
                     href="{{ route('threads.show', $latestPost) }}">{{ $latestPost->title }}</a>
-                <div class="flex text-smaller text-gray-lightest items-center">
-                    <p class="mr-1">Latest:</p>
-                    <profile-popover :user="{{ $latestPost->recentReply->poster }}"
-                        trigger-classes="tet-smaller text-gray-lightest pt-1"></profile-popover>
-                    <p class="dot"></p>
-                    <p> {{ $latestPost->date_updated }} </p>
+                <div class="text-smaller text-gray-lightest items-center w-60">
+                    <span class="mr-1 inline">Latest:</span>
+                    <profile-popover :user="{{ $latestPost->recentReply->poster }}" class="inline"
+                        popover-classes="inline" trigger-classes="tet-smaller text-gray-lightest pt-1">
+                    </profile-popover>
+                    <span class="dot inline-block mb-1"></span>
+                    <span class="inline"> {{ $latestPost->updated_at->diffForHumans() }} </span>
                 </div>
                 <a class="text-smaller text-gray-lightest underline"
                     href="{{ route('categories.show', $latestPost->category->slug) }}">
