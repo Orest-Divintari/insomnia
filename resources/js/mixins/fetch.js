92e8e3498d4e67b08c5b82654687661658b7079e
Orestis Divintari <o.divintari@gmail.com>
2021-04-24 15:59:41 +0200

    Fix: issue with pagination url
    the dataset.next_page_url contains the url that is created from an http request, but the next pages are requested from axios which makes a request to a different url.
    
    the axios requests url start with 'ajax'  and because the first page profile posts and comments are generated by laravel, the 'ajax' prefix is missing

--- resources/js/mixins/fetch.js
+++ resources/js/mixins/fetch.js
@@ -7,8 +7,10 @@ export default {
               .catch((error) => console.log(error));
           },
           fetchMore() {
+            let nextPage = parseInt(this.dataset.current_page)  + 1;
+            console.log(this.path + '?page=' + nextPage);
             axios
-              .get(this.dataset.next_page_url)
+              .get(this.path + '?page=' + nextPage)
               .then(({ data }) => this.refresh(data))
               .catch((error) => console.log(error));
            }
