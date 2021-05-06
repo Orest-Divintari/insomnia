984b3a06dc271a1cad4d06995dfb6db28f1ad729
Orestis Divintari <o.divintari@gmail.com>
2021-05-06 16:35:34 +0200

    Refactor: add option to not jump to content within the page

--- resources/views/components/breadcrumb/leaf.blade.php
+++ resources/views/components/breadcrumb/leaf.blade.php
@@ -1,5 +1,9 @@
 <div class="flex text-sm text-gray-lightest items-center mr-4">
     <li class="">
+        @if(isset($jumpToContent) && $jumpToContent == 'false' )
+        <a href="{{ $route }}" class="hover:underline font-bold">{{ $title }}</a>
+        @else
         <a href="{{ $route . '#' . $title }}" class="hover:underline font-bold">{{ $title }}</a>
+        @endif
     </li>
 </div>
