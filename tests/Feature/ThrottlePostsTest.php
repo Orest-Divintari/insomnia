4df3fa08871401b255d1887ce7def75813b98f58
Orestis Divintari <o.divintari@gmail.com>
2021-05-25 10:39:24 +0200

    Test: ensure that the  first post of a user is not throttled

---
+++ tests/Feature/ThrottlePostsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Middleware\ThrottlePosts;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class ThrottlePostsTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function it_should_not_throttle_the_very_first_post_of_a_user()
+    {
+        $this->withMiddleware(ThrottlePosts::class);
+        $user = $this->signIn();
+        $attributes = [
+            'body' => $this->faker()->sentence(),
+        ];
+
+        $response = $this->postJson(route('ajax.profile-posts.store', $user), $attributes);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+    }
+}
