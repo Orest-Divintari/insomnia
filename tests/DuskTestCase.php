6c85a73beeb7771b23d6a355a927c16e436b9bf2
Orestis Divintari <o.divintari@gmail.com>
2021-09-14 15:15:15 +0200

    Test: disable notifications for all tests

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -5,11 +5,18 @@ namespace Tests;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Support\Facades\Notification;
 use Laravel\Dusk\TestCase as BaseTestCase;
 
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Notification::fake();
+    }
 
     /**
      * Prepare for Dusk test execution.
