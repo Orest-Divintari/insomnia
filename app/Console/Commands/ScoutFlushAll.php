0d82f9c9c9943875d94f797865b5c0ea08b8ce33
Orestis Divintari <o.divintari@gmail.com>
2021-09-21 14:56:01 +0200

    Chore: create a command that flushes all search indices

---
+++ app/Console/Commands/ScoutFlushAll.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ScoutFlushAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scout:flush_all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove all records from all search indices';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call("scout:flush 'App\\\Models\\\Thread'");
+        Artisan::call("scout:flush 'App\\\Models\\\Reply'");
+        Artisan::call("scout:flush 'App\\\Models\\\User'");
+        Artisan::call("scout:flush 'App\\\Models\\\ProfilePost'");
+        Artisan::call("scout:flush 'App\\\Models\\\Tag'");
+    }
+}
