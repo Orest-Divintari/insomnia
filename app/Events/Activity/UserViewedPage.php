5b3e9affa97bdc1b90ac05a2b21e1370454b5a63
Orestis Divintari <o.divintari@gmail.com>
2021-09-21 14:57:56 +0200

    Doc: document the parameters

--- app/Events/Activity/UserViewedPage.php
+++ app/Events/Activity/UserViewedPage.php
@@ -11,12 +11,25 @@ class UserViewedPage
     const CATEGORY = 'Viewing category';
     const REGISTRATION = "Registering";
 
+    /**
+     * The description of the activity
+     *
+     * @var string
+     */
     public $description;
+
+    /**
+     * The specific model that the user is acting on
+     *
+     * @var string|null
+     */
     public $subject;
 
     /**
      * Create a new event instance.
      *
+     * @param string $description
+     * @param string|null $subject
      * @return void
      */
     public function __construct($description, $subject = null)
